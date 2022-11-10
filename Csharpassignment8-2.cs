441071ee849bb935c4f7077f3ada63554cd7c908
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2022-11-10 14:17:06 +0530

    Add files via upload

---
+++ Csharpassignment8-2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary3
+{
+    public class HelloWorld
+    {
+        public string Hello(string name)
+        {
+            return "hello" + name;
+        }
+    }
+}
